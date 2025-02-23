2c0ee420bf1441a374a44422bc7de9a53f43e5d2
Elton133 <134627073+Elton133@users.noreply.github.com>
2025-02-23 22:28:30 +0000

    Changes

--- src/components/tech-stack.jsx
+++ src/components/tech-stack.jsx
@@ -6,36 +6,36 @@ const TechStack = () => {
       <h3 className="text-2xl font-bold mb-4">Tech Stack</h3>
       <div className="flex flex-wrap items-center justify-center md:gap-5 gap-6 max-w-[500px]">
         <img
-          className="md:w-12 md:h-12 w-8 h-8"
+          className="md:w-12 md:h-12 w-6 h-6"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
         />
         <img
-          className="md:w-12 md:h-12 w-8 h-8"
+          className="md:w-12 md:h-12 w-6 h-6"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg"
         />
         <img
-          className="md:w-12 md:h-12 w-8 h-8"
+          className="md:w-12 md:h-12 w-6 h-6"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg"
         />
         <img
-          className="md:w-12 md:h-12 w-8 h-8"
+          className="md:w-12 md:h-12 w-6 h-6"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
         />
         <img
-          className="md:w-12 md:h-12 w-8 h-8"
+          className="md:w-12 md:h-12 w-6 h-6"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
         />
         <img
-          className="md:w-12 md:h-12 w-8 h-8"
+          className="md:w-12 md:h-12 w-6 h-6"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/materialui/materialui-original.svg"
         />
         <img
-          className="md:w-12 md:h-12 w-8 h-8"
+          className="md:w-12 md:h-12 w-6 h-6"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
         />
 
         <img
-          className="md:w-12 md:h-12 w-8 h-8"
+          className="md:w-12 md:h-12 w-6 h-6"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
         />
       </div>
